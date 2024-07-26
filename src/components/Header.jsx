922ebb3eee69975519e9437fa461f296d9c689e6
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-07-26 00:01:20 -0700

    header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-export default function Header() {
+export default function Header(onPageChange) {
   return (
     <>
       <header>
 
             <h2 id="header-paragraph">Hi I'm Sean Madigan. I invite you to join me in shaping the future of the web, one line of code at a time.</h2>
-            <h1 className="main-title"><a href="#about">.Madigan</a></h1>
+            <h1 className="main-title"><a onClick={() => onPageChange('About')}>.Madigan</a></h1>
         </header>
     </>
   );
